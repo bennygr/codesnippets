37391ca48b8ef4f23d5c9ff9ee4975b4689d26c3
benny <bennygr@gmx.net>
2014-09-15 21:22:51 +0200

    * extended required example

--- js/nodejs/require/start.js
+++ js/nodejs/require/start.js
@@ -1,7 +1,9 @@
 var test = require('./testModule.js');
+var test2 = require('./testModule2.js');
 console.log("require tests");
 
 var x = new test.test();
 x.SaySomething("something");
 
 test.testObject.SaySomething("something 2");
+test2();
